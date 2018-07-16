6c63e0095912db49b8a6c1d7afa7fb9a9733687c
akgoode <akgoode@gmail.com>
2018-07-16 16:06:09 -0500

    adds solutions to exercise 10

--- back-end-exercises/exercise10.js
+++ back-end-exercises/exercise10.js
@@ -1,49 +1,68 @@
 // instantiate API context object
-
+const apiContext = require('mozu-node-sdk/clients/platform/application')();
 // define API resources
+const productTypeResource = require('mozu-node-sdk/clients/commerce/catalog/admin/attributedefinition/productType')(apiContext);
+const attributeResource = require('mozu-node-sdk/clients/commerce/catalog/admin/attributedefinition/attribute')(apiContext);
 
 /*
  * Exercise 10.1 INTERACTING WITH PRODUCT TYPES
  */
 
-// create a new productType resource
-// get list of attributes with max page size and starting index at the beginning
-// each product type
-// filter through product type resource and
 // loop through all options
 // output options
-
-/*
- * Exercise 10.2
- */
-
-// create a new productType resource
-
-// get list of attributes with max page size and starting index at the beginning
-
-/*
- * Exercise 10.2
- */
-
-// choice to add attribute as extra, option, or property
-
-// add existing and new extras
-
-// add existing and new options
-
-// add existing and new properties
-//update product type
-
-/*
- * Exercise 10.2
- */
-// create a new attribute resource
-
-// list of attributes
+productTypeResource.getProductTypes({
+    filter: "name eq Footwear"
+})
+    .then(d => {
+        d.items.forEach(item => {
+            item.options.forEach(option => {
+                option.vocabularyValues.forEach(vocabularyValue => {
+                    console.log(vocabularyValue.value);
+                });
+            });
+        });
+    })
+    .catch(e => {
+        console.log(e);
+    });
 
 
 /*
  * Exercise 10.2
+ * Add extra monogram to Footwear product type
  */
 
-// add attribute to attribute in product type
+// helper function
+const logResult = function (data) {
+    console.log(data);
+};
+
+const fetchProductTypeSuccess = function (productTypeData) {
+        // get attribute we want to add to product type
+        attributeResource.getAttribute({
+            attributeFQN: "tenant~monogram"
+        })
+            .then(data => fetchAttributeSuccess(data, productTypeData.items[0]))
+            .catch(logResult);
+};
+
+const fetchAttributeSuccess = function (attributeData, productTypeData) {
+    // adds the attribute to the extras array of the product type
+    productTypeData.extras.push(attributeData);
+
+    // updates the product type with the data we just changed
+    productTypeResource.updateProductType({
+        productTypeId: productTypeData.id
+    }, {
+        body: productTypeData
+    })
+        .then(logResult)
+        .catch(logResult);
+};
+
+// gets product type by name
+productTypeResource.getProductTypes({
+    filter: "name eq Footwear"
+})
+    .then(data => fetchProductTypeSuccess(data))
+    .catch(logResult);
