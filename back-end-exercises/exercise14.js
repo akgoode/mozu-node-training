68f568fcea9989c89415a1a8edbbb2a7dcea33a9
akgoode <akgoode@gmail.com>
2019-09-04 11:20:24 -0500

    exercise 14

---
+++ back-end-exercises/exercise14.js
@@ -0,0 +1,15 @@
+// Exercise 14: Interacting with Orders
+
+// api context object
+// instantiate resources
+
+
+// 1.  Get Existing Orders
+
+// 2.  Update Order Payments
+
+// 3.  Update Order Fulfillment
+
+// 4.  Duplicate an Order (for importing legacy orders)
+
+// 5.  Event Handling (Orders)
