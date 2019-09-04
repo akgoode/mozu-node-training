53dd86982e0842b1770e9d5ccb51c2fb71157e29
akgoode <akgoode@gmail.com>
2019-09-04 16:19:39 -0500

    exercise 15 solutions

--- back-end-exercises/exercise15.js
+++ back-end-exercises/exercise15.js
@@ -1,10 +1,24 @@
 // Exercise 15: Discounts
 
 // api context
-
+const apiContext = require('mozu-node-sdk/clients/platform/application')();
 // api resources
-
+const discountResource = require('mozu-node-sdk/clients/commerce/catalog/admin/discount')(apiContext);
 // 1.
+const discountBody = {
+    "content": {
+        "localeCode": "en-US",
+        "name": "10% Off Order"
+    },
+    "scope": "Order",
+    "target": {
+        "type": "Product"
+    },
+    "amount": 10.0000,
+    "amountType": "Percentage",
+    "status": "Active",
+}
 
-
-// 2.
+discountResource.createDiscount({}, {
+    body: discountBody
+}).then(console.log);
